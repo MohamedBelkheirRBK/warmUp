bdf1df923a2a0c35b5ec72b7e245a3954d762194
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-11-05 09:34:32 +0100

    done warmup 15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,31 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+
+// solved this by first cutting the string into an array manually, identifying word endings by the empty space
+// and using an index counter outside of the loop to keep track
+
+//afterwards join the array back manually by looping through it, in reverse, and adding the element + " "
+//we stop before reaching the first element, so that we can add it in the end without an empty space
+function reverseString(string){
+	var word = '';
+	var result = [];
+	var index = 0;
+	for(var i = 0; i < string.length; i++){
+		if(string[i]===' '){
+			result[index]=word;
+			word = '';
+			index++;
+		}else
+		word+= string[i]
+		if(i === string.length-1)
+		result[index]=word
+	}
+
+	var reversedString = ''
+	for(var i = result.length-1; i>0; i--){
+		reversedString += result[i] + " ";
+	}
+	return reversedString+result[0]
+}
