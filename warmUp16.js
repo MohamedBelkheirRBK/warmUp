898077bb446423e4549841456af4668205793991
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-11-06 09:32:24 +0100

    done with warmup 16

--- warmUp16.js
+++ warmUp16.js
@@ -16,3 +16,20 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+
+function balloon(string){
+	var array = string.split('');
+	var counter = 0;
+	var bal = ['b', 'a', 'l', 'l', 'o', 'o', 'n'];
+	for(var i in array){
+		if(bal.indexOf(array[i])>=0){
+			bal.splice(bal.indexOf(string[i]), 1);
+		}
+
+		if(bal.length===0){
+			counter++;
+			bal = ['b', 'a', 'l', 'l', 'o', 'o', 'n'];
+		}
+	}
+	return counter;
+}
